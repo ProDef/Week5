937782a336284b8475e0f8e6f0e899f70d89a373
Abdi Ali <abdirazak.ali.12@live.com>
2013-10-31 09:30:34 +0000

    changes to mistaesthat have been made

--- sudoku.rb
+++ sudoku.rb
@@ -1,5 +1,24 @@
 require 'sinatra'
+require_relative './lib/sudoku'
+require_relative './lib/cell'
 
-get '/' do 
-	erb :index
+# get '/' do 
+# 	erb :index
+# end
+
+get '/' do
+  @current_solution = random_sudoku
+  erb :index
 end
+
+def random_sudoku
+	seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
+	sudoku = Sudoku.new(seed.join)
+	sudoku.solve!
+	sudoku.to_s.chars
+end
+
+
+
+
+
